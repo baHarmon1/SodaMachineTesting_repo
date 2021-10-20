4f0efc28959b24e981a101bd6cd98583e0b9428a
austin sanchez <austinsanchez45@yahoo.com>
2021-10-20 14:54:46 -0500

    Created the test classes for the 'test coin from register, coded them out and tested them.

--- test_soda_machine.py
+++ test_soda_machine.py
@@ -11,10 +11,45 @@ class TestFillRegister(unittest.TestCase):
         register_length = len(self.soda_machine.register)
         self.assertEqual(88, register_length)
 
-if __name__ == "__main__":
-    unittest.main
-
 class TestFillInventory(unittest.TestCase):
 
     def setUp(self):
         self.soda_machine = SodaMachine()
+
+    def test_inventory_length(self):
+        inventory_length = len(self.soda_machine.inventory)
+        self.assertEqual(30, inventory_length)
+
+class TestGetCoinFromRegister(unittest.TestCase):
+
+    def setUp(self):
+        self.soda_machine = SodaMachine()
+
+    def test_get_coin_register_quarter(self):
+        """Test that 'Quarter' can be returned from the register"""
+        returned_coin = self.soda_machine.get_coin_from_register("Quarter")
+        self.assertEqual("Quarter", returned_coin.name)
+
+    def test_get_coin_register_dime(self):
+        """Test that 'Dime' can be returned from the register"""
+        returned_coin = self.soda_machine.get_coin_from_register("Dime")
+        self.assertEqual("Dime", returned_coin.name)
+
+    def test_get_coin_register_nickle(self):
+        """Test that 'Nickel' can be returned from the register"""
+        returned_coin = self.soda_machine.get_coin_from_register("Nickel")
+        self.assertEqual("Nickel", returned_coin.name)
+
+    def test_get_coin_register_penny(self):
+        """Test that 'Penny' can be returned from the register"""
+        returned_coin = self.soda_machine.get_coin_from_register("Penny")
+        self.assertEqual("Penny", returned_coin.name)
+
+    def test_passing_string(self):
+        """Test the 'None' can be returned from the register"""
+        returned_coin = self.soda_machine.get_coin_from_register("Invalid")
+        self.assertIsNone(returned_coin)
+        
+
+if __name__ == "__main__":
+    unittest.main()
