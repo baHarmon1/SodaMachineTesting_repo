ae5766c0737487f22103c5432b1d51a2b53d792a
austin sanchez <austinsanchez45@yahoo.com>
2021-10-20 12:25:06 -0500

    the test wallet class now properly tests for the length of the wallet.

---
+++ test_wallet.py
@@ -0,0 +1,16 @@
+import unittest
+from wallet import Wallet
+
+class TestFillWallet(unittest.TestCase):
+
+
+    def setUp(self):
+        self.wallet = Wallet()
+
+    def test_wallet_length(self):
+        """Test that you have 88 coins in your wallet."""
+        wallet_length = len(self.wallet.money)
+        self.assertEqual(88, wallet_length)
+
+if __name__ == "__main__":
+    unittest.main()
